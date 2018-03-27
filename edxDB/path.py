68b64dd14b2ad598f8faa66d718819803ea72dbf
James <shingyipcheung@gmail.com>
2018-03-27 17:18:14 +0800

    update path

--- edxDB/path.py
+++ edxDB/path.py
@@ -1,6 +1,19 @@
 from itertools import chain
+from edxDB.df_loader import load_df
+from heapq import nlargest
+from functools import lru_cache
 
-from edxDB.constants import CONCEPT_EDGES
+
+def generate_ratio_dict():
+    risk_ratio = load_df("risk_ratio.pkl")
+    ratio_dict = {}
+    for index, row in risk_ratio.iterrows():
+        v, u, w = row["source"], row["target"], row["value"]
+        if v in ratio_dict:
+            ratio_dict[v][u] = w
+        else:
+            ratio_dict[v] = {u: w}
+    return ratio_dict
 
 
 class Graph:
@@ -53,7 +66,7 @@ class Graph:
 
         def topological_recursive():
             if not unvisited:
-                paths.append(path)
+                paths.append(path.copy())
             else:
                 for node in self.all_nodes:
                     if node in unvisited and self.indegree[node] == 0:
@@ -75,14 +88,56 @@ class Graph:
         return paths
 
 
-def score(path: list) -> float:
-    """
-    :param path: a list of nodes
-    :return: score
-    """
-    print(path)
-    return 0
+def paths_selection(fitness_func, paths: list, top_k=10) -> list():
+    if len(paths) < top_k:
+        top_k = len(paths)
+    fitness = list(map(fitness_func, paths))
+    # sort from high to low
+    nlargest_indexes = nlargest(top_k, range(len(paths)), key=lambda i: fitness[i])
+    # select top k paths
+    return [paths[i] for i in nlargest_indexes]
 
 
-g = Graph(CONCEPT_EDGES)
-print(len(g.topological_all()))
+class PathEvaluator:
+    # static variable for calculation
+    risk_ratio = generate_ratio_dict()
+    mean = load_df("concept_score_mean.pkl")
+    student_grade = load_df("student_concept_grade.pkl")
+
+    def __init__(self, student_id):
+        self.student_id = student_id
+        mean = PathEvaluator.mean
+        # diff between student score and mean
+        self.relative_score = (mean - self.score().fillna(0)) / mean
+
+    def score(self):
+        return PathEvaluator.student_grade.loc[self.student_id]
+
+    @lru_cache(maxsize=256)
+    def node_position_score(self, position, node, n):
+        # parameter B from Wenlong
+        return (n - position) * self.relative_score.ix[node]
+
+    def evaluate(self, path: list) -> float:
+        """
+        :param path:
+        :return: path score
+        """
+        n = len(path)
+        sum_of_product = 0
+        for i, node in enumerate(path):
+            sum_of_product += self.node_position_score(i, node, n)
+        return sum_of_product
+
+
+def main():
+    from edxDB.constants import CONCEPT_EDGES
+    graph = Graph(CONCEPT_EDGES)
+    paths = graph.topological_all()
+    evaluator = PathEvaluator(student_id=69845)
+    top_k_paths = paths_selection(evaluator.evaluate, paths, top_k=5)
+    print(top_k_paths)
+
+
+if __name__ == '__main__':
+    main()
